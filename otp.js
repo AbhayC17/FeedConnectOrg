fdace414c6b37deb7735df46a651b36b17eaf4a0
FeedConnect <abhay.chandrik.1729@gmail.com>
2025-06-14 08:01:37 +0530

    Update otp.js

--- otp.js
+++ otp.js
@@ -1,29 +1,35 @@
-function sendDonorOTP() {
-  const email = document.querySelector('#donorForm input[name="email"]').value;
-  const name = document.querySelector('#donorForm input[name="name"]').value;
-
-  fetch("https://script.google.com/macros/s/AKfycbxLBNeKJgaseTYlctme7jqoHtzueRpGjZTE1EvboQJryZif9JsTTJEM65S464HfZ-OT/exec", {
+function sendOTP(email, name) {
+  fetch("YOUR_WEB_APP_URL", {
     method: "POST",
     body: JSON.stringify({
       type: "sendOTP",
       email: email,
       name: name
     }),
-    headers: { "Content-Type": "application/json" }
+    headers: {
+      "Content-Type": "application/json"
+    }
   }).then(res => res.text()).then(alert);
 }
 
-function sendOrgOTP() {
-  const email = document.querySelector('#orgForm input[name="email"]').value;
-  const name = document.querySelector('#orgForm input[name="personName"]').value;
-
-  fetch("https://script.google.com/macros/s/AKfycbxLBNeKJgaseTYlctme7jqoHtzueRpGjZTE1EvboQJryZif9JsTTJEM65S464HfZ-OT/exec", {
+function verifyOTP(email, otp, callback) {
+  fetch("YOUR_WEB_APP_URL", {
     method: "POST",
     body: JSON.stringify({
-      type: "sendOTP",
+      type: "verifyOTP",
       email: email,
-      name: name
+      otp: otp
     }),
-    headers: { "Content-Type": "application/json" }
-  }).then(res => res.text()).then(alert);
+    headers: {
+      "Content-Type": "application/json"
+    }
+  }).then(res => res.text()).then(response => {
+    if (response === "success") {
+      alert("OTP Verified!");
+      callback();
+    } else {
+      alert("Invalid OTP.");
+    }
+  });
 }
+
