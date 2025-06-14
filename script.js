9733bd09bcba5bb041d5fed38c1382111e8faca7
FeedConnect <abhay.chandrik.1729@gmail.com>
2025-06-14 08:28:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,20 @@ function submitForm(formId, type) {
   const form = document.getElementById(formId);
   form.addEventListener('submit', function(e) {
     e.preventDefault();
+
     const formData = new FormData(form);
     const json = {};
     formData.forEach((v, k) => json[k] = v);
-    fetch("https://script.google.com/macros/s/AKfycbxLBNeKJgaseTYlctme7jqoHtzueRpGjZTE1EvboQJryZif9JsTTJEM65S464HfZ-OT/exec?type=" + type, {
-      method: 'POST',
-      body: JSON.stringify(json)
-    }).then(res => res.text()).then(alert);
+
+    const email = json.email;
+    const otp = json.otp;
+
+    verifyOTP(email, otp, () => {
+      fetch("https://script.google.com/macros/s/AKfycbxLBNeKJgaseTYlctme7jqoHtzueRpGjZTE1EvboQJryZif9JsTTJEM65S464HfZ-OT/exec?type=" + type, {
+        method: 'POST',
+        body: JSON.stringify(json)
+      }).then(res => res.text()).then(alert);
+    });
   });
 }
 
